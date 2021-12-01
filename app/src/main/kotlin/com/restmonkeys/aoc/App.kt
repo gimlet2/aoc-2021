30f60ae282d3cded169a5b086c4a4ccab8c6dfea
Andrey <andrey.chernishov@gmail.com>
2021-12-01 15:52:10 +0000

    extract file reading

--- app/src/main/kotlin/com/restmonkeys/aoc/App.kt
+++ app/src/main/kotlin/com/restmonkeys/aoc/App.kt
@@ -6,24 +6,22 @@ package com.restmonkeys.aoc
 import java.io.File
 
 fun main() {
-    
-    print(`task1 part2`())
+    val input: List<Int> = File("/workspaces/aoc-2021/1_1.txt").readLines().map { it.toInt() }
+    print(`task1 part2`(input))
 }
 
-fun `task1 part1`(): Int {
-    val input: List<Int> = File("/workspaces/aoc-2021/1_1.txt").readLines().map { it.toInt()}
+fun `task1 part1`(input: List<Int>): Int {
     var result = 0
-    for(i in 1..(input.size-1)) {
-        if(input[i] > input[i-1]) result++
+    for (i in 1..(input.size - 1)) {
+        if (input[i] > input[i - 1]) result++
     }
     return result
 }
 
-fun `task1 part2`(): Int {
-    val input: List<Int> = File("/workspaces/aoc-2021/1_1.txt").readLines().map { it.toInt()}
+fun `task1 part2`(input: List<Int>): Int {
     var result = 0
-    for(i in 3..(input.size-1)) {
-        if(input[i] - input[i-3] > 0) result++
+    for (i in 3..(input.size - 1)) {
+        if (input[i] - input[i - 3] > 0) result++
     }
     return result
 }
