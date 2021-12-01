3461c9292da3e0d7d016d8a6766ef9ba5ed68099
Andrey <andrey.chernishov@gmail.com>
2021-12-01 15:36:47 +0000

    task 1 part 2 is done

--- app/src/main/kotlin/com/restmonkeys/aoc/App.kt
+++ app/src/main/kotlin/com/restmonkeys/aoc/App.kt
@@ -6,10 +6,27 @@ package com.restmonkeys.aoc
 import java.io.File
 
 fun main() {
+    
+    print(`task1 part2`())
+}
+
+fun `task1 part1`(): Int {
     val input: List<Int> = File("/workspaces/aoc-2021/1_1.txt").readLines().map { it.toInt()}
     var result = 0
     for(i in 1..(input.size-1)) {
         if(input[i] > input[i-1]) result++
     }
-    print(result)
+    return result
+}
+
+fun `task1 part2`(): Int {
+    val input: List<Int> = File("/workspaces/aoc-2021/1_1.txt").readLines().map { it.toInt()}
+    var result = 0
+    for(i in 3..(input.size-1)) {
+        val first = input[i-3] + input[i-2] + input[i-1]
+        val second = input[i-2] + input[i-1] + input[i]
+        
+        if(second > first) result++
+    }
+    return result
 }
